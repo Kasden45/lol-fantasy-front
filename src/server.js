5a556aceed377e3971e3858f22a2c67e956b2428
Konrad Liuras <kliuras@farutex.pl>
2024-10-06 12:39:59 +0200

    fix config

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const express = require('express')
 // create server instance
 const app = express()
 // bind the request to an absolute path or relative to the CWD
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static('dist'))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join('dist', 'index.html'));
   });
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`))
