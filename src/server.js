c21abcae1965dda5ec3353a6ce85d303fb68c7c6
Konrad Liuras <kliuras@farutex.pl>
2024-10-06 12:53:03 +0200

    try new config with fixed path

--- src/server.js
+++ src/server.js
@@ -6,10 +6,10 @@ const path = require('path');
 // create server instance
 const app = express()
 // bind the request to an absolute path or relative to the CWD
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, '../dist')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
   });
 // start the server
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port} and ${path.join(__dirname, '../dist')}`))
