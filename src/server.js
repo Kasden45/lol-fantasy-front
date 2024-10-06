59cc23a6fb2cebd8070076ff6fc3677020b2b5f3
Konrad Liuras <kliuras@farutex.pl>
2024-10-06 12:41:50 +0200

    add path import

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ const port = process.env.PORT || 8080
 
 // wire up the module
 const express = require('express')
+const path = require('path');
 // create server instance
 const app = express()
 // bind the request to an absolute path or relative to the CWD
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join('dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 // start the server
 app.listen(port, () => console.log(`Listening on port ${port}`))
