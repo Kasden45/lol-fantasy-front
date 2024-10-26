290091d8377a93d0ef24811e5f9fa0e49ce60192
Konrad Liuras <kliuras@farutex.pl>
2024-10-26 12:36:28 +0200

    add production socket

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 const port = process.env.PORT || 8080
-const socket = io(`https://fantasy-2kpi-2d84171087be.herokuapp.com:${port}`);
+const socket = io(`https://fantasy-2kpi-2d84171087be.herokuapp.com`);
+// const socket = io(`http://localhost:${port}`);
 
 export default socket;
