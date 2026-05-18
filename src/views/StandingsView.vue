b3847a0251ff15abe8ff7a7fbc9d400386ffdbbc
Konrad Liuras <kliuras@farutex.pl>
2026-05-19 00:40:29 +0200

    feat: add StandingsView page

---
+++ src/views/StandingsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="standings-back">
+    <div class="container py-4">
+      <h4 class="text-white mb-4">Standings</h4>
+      <StandingsComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+import StandingsComponent from "@/components/Standings/StandingsComponent.vue";
+
+export default {
+  name: "StandingsView",
+  components: { StandingsComponent },
+};
+</script>
+
+<style scoped>
+.standings-back {
+  background-color: #0a0a0a;
+  min-height: 100vh;
+  color: white;
+}
+</style>
